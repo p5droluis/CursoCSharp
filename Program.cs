ae9c143c20583d3af2bad7bc1166dcf16bdd3828
Pedro Machado <pedro.machado04@hotmail.com>
2022-10-24 11:08:06 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //Diretiva - indica o uso de um determinado conjunto de recursos para a construção e execução de um programa.
+
+// Indicativo de referencia - Todos os arquivos e códigos referentes a um programa estão "armazenados" neste namespace.
+namespace Conceitos.CSHARP.CAP01
+{
+    class Program //
+    {
+        static void Main(string[] args) // Todo o conjunto de instruções para executar as tarefas do programa.
+        {
+            // Tentativa de exebição de um conteudo via console do VS.
+            Console.WriteLine("Olá Mundo C#! Aqui, começa o estudo da base da lp.");
+            
+            //Funcão de leitura de tecla, que para a execução. 
+            Console.ReadKey();
+            }
+    }
+}
